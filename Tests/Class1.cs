00c2b364936d18cf2de3c5fd7ca2252569d5767b
t.boettcher-holschen <t.boettcher-holschen@de.zetes.com>
2012-01-18 19:28:57 +0100

    Welcome Message
    ViewName Test

--- Tests/Class1.cs
+++ Tests/Class1.cs
@@ -2,10 +2,35 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using NUnit.Framework;
 
 namespace Tests
 {
+    using System.Web.Mvc;
+
+    using blogKata.Controllers;
+
+    [TestFixture]
     public class Class1
     {
+
+        [Test]
+        public void WelcomeMessage()
+        {
+            var homeController = new HomeController();
+            var result = homeController.Index() as ViewResult;
+            Assert.AreEqual("Welcome to my blog", result.ViewBag.Title);
+
+        }
+        
+        [Test]
+        public void Index_Action_Should_Return_Index_View()
+        {
+            var homeController = new HomeController();
+            var result = homeController.Index() as ViewResult;
+            Assert.AreEqual(string.Empty, result.ViewName);
+
+        }
+
     }
 }
---
+++ blogKata/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace blogKata.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Welcome to my blog";
+            
+            return View();
+        }
+
+        
+    }
+}
